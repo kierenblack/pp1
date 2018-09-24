150ef55c8537b0df7fb1b0856cc4b68725599fcb
Kieren Black <dunnersguy@gmail.com>
2018-09-24 15:10:10 +1200

    Added a quick sample menu inside a mwthod

--- Program/PP1/Program.cs
+++ Program/PP1/Program.cs
@@ -7,7 +7,21 @@ namespace PP1
     {
         static void Main()
         {
+            
+            
+        }
 
+        static char Menu1()
+        {
+            Console.WriteLine("Hello. what brings you here today?");
+            Console.WriteLine("Press 1 for issue");
+            Console.WriteLine("\t2 for problem");
+            Console.WriteLine("\t3 for bullying");
+            char response = Convert.ToChar(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+            Console.WriteLine(response);
+            Console.ReadLine();
+            return response;
         }
     }
 }
