29ef86d39ef9185743216f1db6d182bc463eabb4
Kieren Black <dunnersguy@gmail.com>
2018-09-24 14:48:32 +1200

    Update 1

--- Program/PP1/Program.cs
+++ Program/PP1/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace PP1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
         }
     }
 }
